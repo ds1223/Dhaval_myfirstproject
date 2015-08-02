510e34e623241c48f0328e9af77e76b894ce995e
ds1223 <dsheth1223@gmail.com>
2015-08-02 07:35:20 -0500

    User Input code
    Code for having user input

---
+++ Userinputnumber.java
@@ -0,0 +1,27 @@
+package codes;
+
+import java.security.PublicKey;
+import java.util.Scanner;
+
+public class Userinputnumber {
+	
+	public static void main(String[] args) {
+		System.out.println("Please enter your grade: ");
+		input();
+		
+	}public static void input() {
+		Scanner in = new Scanner(System.in);
+		
+		int x= in.nextInt(); //Int the i has to be capitalized
+		if (x>90){
+			System.out.println("You received an A");
+		}else if (x>80){
+			System.out.println("You received an B");
+			
+		}else if (x>70){
+			System.out.println("You received an C");
+		}else {
+			System.out.println("See me after class");
+		}
+	}
+}
